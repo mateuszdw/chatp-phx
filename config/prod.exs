2f915079fb2315551ded3aa9a2e3e693fb977a35
MD <mateusz.dworakowski@hand2band.media>
2020-04-25 11:28:01 +0200

    uncomment port 4000; ignore static files

--- config/prod.exs
+++ config/prod.exs
@@ -16,8 +16,8 @@ use Mix.Config
 config :chatplayer, ChatplayerWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "0.0.0.0", port: 4000],
-  check_origin: false,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  check_origin: false
+  # cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
